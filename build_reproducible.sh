328aef13970b750dad650e6b2593dbcf9edcb62b
The SunShining <thesunshiningmin@gmail.com>
2025-03-07 07:28:35 -0700

    Update build_reproducible.sh

--- build_reproducible.sh
+++ build_reproducible.sh
@@ -4,7 +4,7 @@
 cargo near abi
 
 # Build reproducible WASM
-cargo near build-reproducible-wasm
+cargo near build reproducible-wasm
 
 # Create build_near directory if it doesn't exist
 mkdir -p build_near
